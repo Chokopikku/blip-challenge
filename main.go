f3c805bcaebe1a256855471f3872be6dac3b611b
Chokopikku <fpprofissional@gmail.com>
2025-04-04 17:24:36 +0100

    Improved logger

--- main.go
+++ main.go
@@ -4,32 +4,48 @@ import (
 	"chokopikku/blip-challenge/services"
 	"chokopikku/blip-challenge/utils"
 	"fmt"
-	"log"
 )
 
 func main() {
-	utils.SetupLogger()
+	logger, err := utils.NewLogger("app.log")
+	if err != nil {
+		fmt.Printf("Failed to initialize logger: %v\n", err)
+		return
+	}
+	defer logger.Close()
+
+	logger.Info("Application started")
 
 	reader := services.NewCommitReader("commits.csv")
 	commits, err := reader.ReadCommits()
 	if err != nil {
-		log.Fatalf("Error reading commits: %v", err)
+		logger.Error(fmt.Sprintf("Error reading commits: %v", err))
+		return
 	}
+
+	logger.Info("Data reading completed")
 
 	validator := services.NewCommitValidator()
 	for _, commit := range commits {
 		if err := validator.Validate(commit, false); err != nil {
-			log.Printf("Invalid commit: %v", err)
+			logger.Warn(fmt.Sprintf("Invalid commit: %v", err))
 			continue
 		}
 	}
+
+	logger.Info("Data validation completed")
 
 	scorer := services.NewActivityScorer()
 	ranker := services.NewRepositoryRanker()
 	ranking := ranker.Rank(commits, scorer)
 
+	logger.Info("Ranking calculation completed")
+
 	fmt.Println("Top 10 Most Active Repositories:")
 	for i, repo := range ranking {
 		fmt.Printf("%d. %s - Activity Score: %.2f\n", i+1, repo.Name, repo.Score)
+		logger.Debug(fmt.Sprintf("%d. %s - Activity Score: %.2f\n", i+1, repo.Name, repo.Score))
 	}
+
+	logger.Info("Application finished")
 }
--- utils/logger.go
+++ utils/logger.go
@@ -1,11 +1,61 @@
 package utils
 
 import (
+	"errors"
 	"log"
 	"os"
 )
 
-func SetupLogger() {
-	log.SetOutput(os.Stdout)
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+// Log levels
+const (
+	LevelInfo  = "INFO"
+	LevelDebug = "DEBUG"
+	LevelWarn  = "WARN"
+	LevelError = "ERROR"
+)
+
+// CustomLogger is a log.Logger wrapper, with levels, that outputs logs to a file.
+type CustomLogger struct {
+	logger  *log.Logger
+	logFile *os.File
+}
+
+func NewLogger(filePath string) (*CustomLogger, error) {
+	if filePath == "" {
+		return nil, errors.New("log file path must not be empty")
+	}
+
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	logger := log.New(file, "", log.Ldate|log.Ltime|log.Lshortfile)
+	return &CustomLogger{logger: logger, logFile: file}, nil
+}
+
+func (l *CustomLogger) Info(message string) {
+	l.log(LevelInfo, message)
+}
+
+func (l *CustomLogger) Debug(message string) {
+	l.log(LevelDebug, message)
+}
+
+func (l *CustomLogger) Warn(message string) {
+	l.log(LevelWarn, message)
+}
+
+func (l *CustomLogger) Error(message string) {
+	l.log(LevelError, message)
+}
+
+func (l *CustomLogger) log(level string, message string) {
+	l.logger.Printf("[%s] %s", level, message)
+}
+
+func (l *CustomLogger) Close() {
+	if l.logFile != nil {
+		_ = l.logFile.Close()
+	}
 }
